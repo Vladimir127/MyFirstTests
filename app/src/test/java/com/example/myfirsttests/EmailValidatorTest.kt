5bc83371f61446429734575fd58d4559e9a2f35c
Vladimir Stepanov <vladimir-stepanov-home@yandex.ru>
2023-02-15 17:09:49 +0300

    Добавлены тесты для следующих случаев: - кириллические символы в адресе - нет домена - присутствуют специальные символы - присутствуют пробелы - слишком длинное имя пользователя - слишком длинное доменное имя 1-го уровня - слишком длинное доменное имя 2-го уровня

--- app/src/test/java/com/example/myfirsttests/EmailValidatorTest.kt
+++ app/src/test/java/com/example/myfirsttests/EmailValidatorTest.kt
@@ -40,4 +40,39 @@ class EmailValidatorTest {
     fun emailValidator_NullEmail_ReturnsFalse() {
         assertFalse(EmailValidator.isValidEmail(null))
     }
+
+    @Test
+    fun emailValidator_CyrillicLetters_ReturnsFalse() {
+        assertFalse(EmailValidator.isValidEmail("володя@email.com"))
+    }
+
+    @Test
+    fun emailValidator_NoDomain_ReturnsFalse() {
+        assertFalse(EmailValidator.isValidEmail("example.com"))
+    }
+
+    @Test
+    fun emailValidator_SpecialSymbol_ReturnsFalse() {
+        assertFalse(EmailValidator.isValidEmail("example?@email.com"))
+    }
+
+    @Test
+    fun emailValidator_ContainsSpaces_ReturnsFalse() {
+        assertFalse(EmailValidator.isValidEmail("my email address@email.com"))
+    }
+
+    @Test
+    fun emailValidator_TooLongDomainFirstLevel_ReturnsFalse() {
+        assertFalse(EmailValidator.isValidEmail("example@email.qwertyuiopasdfghjklzxcvbnmqwerty"))
+    }
+
+    @Test
+    fun emailValidator_TooLongDomainSecondLevel_ReturnsFalse() {
+        assertFalse(EmailValidator.isValidEmail("example@qwertyuiopasdfghjklzxcvbnmqwertyuiopasdfghjklzxcvbnmqwertyuiopasdfghjklzxcvbnm.com"))
+    }
+
+    @Test
+    fun emailValidator_TooLongUserName_ReturnsFalse() {
+        assertFalse(EmailValidator.isValidEmail("qwertyuiopasdfghjklzxcvbnmqwertyuiopasdfghjklzxcvbnmqwertyuiopasdfghjklzxcvbnmqwertyuiopasdfghjklzxcvbnmqwertyuiopasdfghjklzxcvbnmqwertyuiopasdfghjklzxcvbnmqwertyuiopasdfghjklzxcvbnmqwertyuiopasdfghjklzxcvbnmqwertyuiopasdfghjklzxcvbnmqwertyuiopasdfghjklzxcvbnmqwertyuiopasdfghjklzxcvbnmqwertyuiopasdfghjklzxcvbnm@email.com"))
+    }
 }
